2cda41e08fccae210607005fbfe44473b40a9e9e
ilikeavocadoes <ilikeavocadoes@hush.com>
2021-04-01 12:59:12 +0300

    Update Pytype version to 2021.3.22

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-    
+
 from setuptools import setup
 
 
 setup(
     name='pre_commit_dummy_package',
     version='1.0.0',
-    install_requires=['pytype==2020.10.8'],
+    install_requires=['pytype==2021.3.22'],
+
 )
